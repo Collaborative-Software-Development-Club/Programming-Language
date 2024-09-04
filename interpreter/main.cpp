b844b437db4be085c12ee51e02565809ee235e47
Tristan Leedy <117492192+eldenleedy@users.noreply.github.com>
2024-09-04 19:10:37 -0400

    Scanner & main updates

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -4,15 +4,60 @@
 
 using namespace std;
 
+// Helper function to display the name of the token for debugging
+string token_to_string(simple token) {
+    switch (token) {
+        case PROGRAM: return "PROGRAM";
+        case NAME: return "NAME";
+        case ASSIGN: return "ASSIGN";
+        case SEMICOLON: return "SEMICOLON";
+        case PRINT: return "PRINT";
+        case IF: return "IF";
+        case ELSE: return "ELSE";
+        case WHILE: return "WHILE";
+        case LPAREN: return "LPAREN";
+        case RPAREN: return "RPAREN";
+        case LCURL: return "LCURL";
+        case RCURL: return "RCURL";
+        case NOT: return "NOT";
+        case OR: return "OR";
+        case AND: return "AND";
+        case EQUAL: return "EQUAL";
+        case LESS: return "LESS";
+        case ADD: return "ADD";
+        case SUBTRACT: return "SUBTRACT";
+        case MULTIPLY: return "MULTIPLY";
+        case DIVIDE: return "DIVIDE";
+        case NUMBER: return "NUMBER";
+        case NUM: return "NUM";
+        case EOS: return "EOS";
+        default: return "UNKNOWN";
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc > 1) {
-        Scanner s (argv[1]);
+        Scanner scanner(argv[1]);
 
-        // write code here to ensure your tokens are being processed correctly!
+        // Scan through tokens in the file
+        scanner.next_token();
+        while (scanner.current_token() != EOS) {
+            // Display the token type
+            cout << "Token: " << token_to_string(scanner.current_token()) << endl;
+
+            // If the token is NAME or NUMBER, display the corresponding value
+            if (scanner.current_token() == NAME) {
+                cout << "Name: " << scanner.get_name() << endl;
+            } else if (scanner.current_token() == NUMBER) {
+                cout << "Number: " << scanner.get_number() << endl;
+            }
+
+            // Move to the next token
+            scanner.next_token();
+        }
 
         return 0;
-    }
-    else { // in case our code has no arguments
+    } else {
         cerr << "ERROR: No file argument specified!" << endl;
         return -1;
     }
--- interpreter/scanner.cpp
+++ interpreter/scanner.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <cctype>
+#include <vector>
 #include "simple.cpp"
 
 using namespace std;
@@ -8,43 +10,173 @@ using namespace std;
 #define SCANNER_H
 
 class Scanner {
-    private:
+private:
+    vector<simple> tokens; // Stores all identified tokens
+    vector<char> stream; // Stores the input character stream
+    simple curr_token; // Current token being processed
+    int curr_number; // Current number if the token is a number
+    string curr_name; // Current name if the token is a name
+    size_t stream_pos = 0; // Current position in the stream
 
-        // you may either keep an array or a list of all characters in the stream, or...
-        // ...keep the file stream open, and read only characters that you need at the moment!
+    // Helper function to skip whitespace
+    void skip_whitespace() {
+        while (stream_pos < stream.size() && isspace(stream[stream_pos])) {
+            stream_pos++;
+        }
+    }
 
-        simple curr_token; // the current token
-        int curr_number; // the int value of the most recent (or current) 'NUMBER' token
-        string curr_name; // the string value of the most recent (or current) 'NAME' token
+    // Helper function to check for valid letters in names
+    bool is_name_char(char c) {
+        return isalpha(c) || c == '_';
+    }
 
-    public:
-        Scanner(string f) {
-            // use filename argument 'f' to open the file, read characters as needed, and eventually close
-            // the file stream
+    // Helper function to read a name
+    string read_name() {
+        string name = "";
+        while (stream_pos < stream.size() && is_name_char(stream[stream_pos])) {
+            name += stream[stream_pos];
+            stream_pos++;
+        }
+        return name;
+    }
 
-            // if we cannot find the specified file, gracefully return an error and exit
+    // Helper function to read a number
+    int read_number() {
+        int num = 0;
+        while (stream_pos < stream.size() && isdigit(stream[stream_pos])) {
+            num = num * 10 + (stream[stream_pos] - '0');
+            stream_pos++;
+        }
+        return num;
+    }
+
+public:
+    Scanner(string filename) {
+        ifstream file(filename);
+        if (!file) {
+            cerr << "ERROR: Cannot open file: " << filename << endl;
+            exit(EXIT_FAILURE);
         }
 
-        simple current_token() {
-            // all you have to do here is return the currently stored token without going to the next one
-            return EOS;
+        // Read file contents into stream
+        char c;
+        while (file.get(c)) {
+            stream.push_back(c);
+        }
+        file.close();
+    }
+
+    // Returns the current token without advancing
+    simple current_token() {
+        return curr_token;
+    }
+
+    // Moves to the next token in the input stream
+    void next_token() {
+        skip_whitespace(); // Skip whitespace before reading the next token
+
+        if (stream_pos >= stream.size()) {
+            curr_token = EOS;
+            return;
         }
 
-        void next_token() {
-            // find the next available token in the scanning stream, covert it into a token, store it
-            // if a character is an invalid symbol, throw an error
-            // NOTE: this will be your longest method!
-        }
+        char c = stream[stream_pos];
 
-        string get_name() {
-            // if the current token is 'NAME', return the string value of the name, else throw an error
-            return "";
+        if (is_name_char(c)) {
+            // Handle names (like PROGRAM, IF, ELSE, or user-defined variables)
+            curr_name = read_name();
+            if (curr_name == "PROGRAM") curr_token = PROGRAM;
+            else if (curr_name == "PRINT") curr_token = PRINT;
+            else if (curr_name == "IF") curr_token = IF;
+            else if (curr_name == "ELSE") curr_token = ELSE;
+            else if (curr_name == "WHILE") curr_token = WHILE;
+            else if (curr_name == "NUM") curr_token = NUM;
+            else curr_token = NAME;
+        } else if (isdigit(c)) {
+            // Handle numbers
+            curr_number = read_number();
+            curr_token = NUMBER;
+        } else {
+            // Handle single character tokens and operators
+            switch (c) {
+                case '=':
+                    curr_token = ASSIGN;
+                    stream_pos++;
+                    break;
+                case ';':
+                    curr_token = SEMICOLON;
+                    stream_pos++;
+                    break;
+                case '(':
+                    curr_token = LPAREN;
+                    stream_pos++;
+                    break;
+                case ')':
+                    curr_token = RPAREN;
+                    stream_pos++;
+                    break;
+                case '{':
+                    curr_token = LCURL;
+                    stream_pos++;
+                    break;
+                case '}':
+                    curr_token = RCURL;
+                    stream_pos++;
+                    break;
+                case '!':
+                    curr_token = NOT;
+                    stream_pos++;
+                    break;
+                case '|':
+                    curr_token = OR;
+                    stream_pos++;
+                    break;
+                case '&':
+                    curr_token = AND;
+                    stream_pos++;
+                    break;
+                case '<':
+                    curr_token = LESS;
+                    stream_pos++;
+                    break;
+                case '+':
+                    curr_token = ADD;
+                    stream_pos++;
+                    break;
+                case '-':
+                    curr_token = SUBTRACT;
+                    stream_pos++;
+                    break;
+                case '*':
+                    curr_token = MULTIPLY;
+                    stream_pos++;
+                    break;
+                case '/':
+                    curr_token = DIVIDE;
+                    stream_pos++;
+                    break;
+                default:
+                    cerr << "ERROR: Unrecognized token: " << c << endl;
+                    exit(EXIT_FAILURE);
+            }
         }
+    }
 
-        int get_number() {
-            // if the current token is 'NUMBER', return the int value of the number, else throw an error
-            return 0;
+    // Get the current name token value
+    string get_name() {
+        if (curr_token == NAME) {
+            return curr_name;
         }
+        throw runtime_error("ERROR: Current token is not NAME");
+    }
+
+    // Get the current number token value
+    int get_number() {
+        if (curr_token == NUMBER) {
+            return curr_number;
+        }
+        throw runtime_error("ERROR: Current token is not NUMBER");
+    }
 };
 
 #endif
