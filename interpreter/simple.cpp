9d9e83b9a8f968e2ed97244783ee211549191aaa
charlie-siri7 <charles.sirich@gmail.com>
2024-09-11 18:54:40 -0400

    added comments for keywords

--- interpreter/simple.cpp
+++ interpreter/simple.cpp
@@ -2,30 +2,30 @@
 #define SIMPLE_H
 
 enum simple {
-    PROGRAM,
-    NAME,
-    ASSIGN,
-    SEMICOLON,
-    PRINT,
-    IF,
-    ELSE,
-    WHILE,
-    LPAREN,
-    RPAREN,
-    LCURL,
-    RCURL,
-    NOT,
-    OR,
-    AND,
-    EQUAL,
-    LESS,
-    ADD,
-    SUBTRACT,
-    MULTIPLY,
-    DIVIDE,
-    NUMBER,
-    NUM,
-    EOS
+    PROGRAM, // memoranda
+    NAME, // designation
+    ASSIGN, // designate
+    SEMICOLON, // .
+    PRINT, // etch
+    IF, // presuming
+    ELSE, // lest
+    WHILE, // midst
+    LPAREN, // (
+    RPAREN, // )
+    LCURL, // {
+    RCURL, // }
+    NOT, // nay
+    OR, // disjunction
+    AND, // moreover
+    EQUAL, // commensurate
+    LESS, // beneath
+    ADD, // replenish
+    SUBTRACT, // diminish
+    MULTIPLY, // mushroom
+    DIVIDE, // dichotomize
+    NUMBER, // numeral
+    NUM, // 
+    EOS // conclusion
 };
 
 #endif
