9d19590c1af3490b58cc3065a6b08733e5948104
charlie-siri7 <charles.sirich@gmail.com>
2024-09-18 18:55:36 -0400

    Update scanner.cpp

--- interpreter/scanner.cpp
+++ interpreter/scanner.cpp
@@ -22,6 +22,30 @@ class Scanner {
         int curr_number; // the int value of the most recent (or current) 'NUMBER' token
         string curr_name; // the string value of the most recent (or current) 'NAME' token
 
+        bool isNumber(string str) {
+            bool isNumber = true;
+            for (char c : str) {
+                if (!isdigit(c)) {
+                    isNumber = false;
+                }
+            }
+            return isNumber;
+        }
+
+        bool isLetter(string str) {
+            bool isLetter = true;
+            for (char c : str) {
+                if (!isalpha(c)) {
+                    isLetter = false;
+                }
+            }
+            return isLetter;
+        }
+
+        bool isSymbol(string str) {
+            return (!isNumber(str) && !isLetter(str));
+        }
+
         bool checkSyntax(string s){
             if(s.compare("designation") == 0){ curr_token = NAME; }
             else if(s.compare("designate") == 0) { curr_token = ASSIGN;}
@@ -93,6 +117,53 @@ class Scanner {
             // find the next available token in the scanning stream, covert it into a token, store it
             // if a character is an invalid symbol, throw an error
             // NOTE: this will be your longest method!
+            string token_string = "";
+            char token_c;
+            while (curr_token != EOS) {
+                token_c = charStream[charPointer];
+                if (charPointer != charStream.size() && token_c != ' ' && token_c != '\n' && token_c != '\0' && token_c != '\t') { // checking for any potential whitespace
+                    token_string += token_c; // if not whitespace, append to string
+                    if (!isdigit(charStream[charPointer + 1]) && !isalpha(charStream[charPointer + 1]) && checkKeyword(token_string)) {
+                        // if the current word looks like a keyword and has nothing leading it, it's a keyword
+                        charPointer++;
+                        break;
+                    }
+                    else if ((!isdigit(charStream[charPointer + 1]) && !isalpha(charStream[charPointer + 1])) || isSymbol(token_string)) { // check if we still have valid chars ahead
+                        if (isNumber(token_string)) { // check if a full number
+                            curr_token = NUMBER;
+                            curr_number = stoi(token_string);
+                            if (curr_number < 0 || curr_number > 1000000) { // check for too big
+                                cerr << "ERROR: NUMBER value too large!" << endl;
+                                exit(-1);
+                            }
+                            charPointer++;
+                            break;
+                        }
+                        else if (isSymbol(token_string)) {
+                            if (token_c == '=' && charStream[charPointer + 1] == '=') { // i only need to check '==', otherwise it's one character symbol
+                                curr_token = EQUAL;
+                                charPointer += 2;
+                                break;
+                            }
+                            else {
+                                checkSyntax(token_string);
+                                charPointer++;
+                                break;
+                            }
+                        }
+                        else { // anything else is a variable name
+                            curr_token = NAME;
+                            curr_name = token_string;
+                            charPointer++;
+                            break;
+                        }
+                    }
+                }
+                else if (charPointer >= charStream.size() - 1) { // get eos token here
+                    curr_token = EOS;
+                }
+                charPointer++;
+            }
         }
 
         string get_name() {
