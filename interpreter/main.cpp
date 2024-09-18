f8180bc24dde7090351855886b24ee774ce0f5a3
holmes301 <quintonrholmes30@gmail.com>
2024-09-18 00:42:26 -0400

    Code skeletons updated!

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <string>
 #include "scanner.cpp"
+#include "parser.cpp"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
     if (argc > 1) {
-        Scanner s (argv[1]);
-
-        // write code here to ensure your tokens are being processed correctly!
-
-        return 0;
+        Parser::initialize_parser(argv[1]);
+        Parser::parse_all();
+        Parser::print_all();
     }
     else { // in case our code has no arguments
         cerr << "ERROR: No file argument specified!" << endl;
