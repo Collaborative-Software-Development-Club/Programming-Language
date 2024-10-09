3c7c85aedf2e5b905f38df192e92d01f5ecd3ca0
Megumi <noodlemegumi@gmail.com>
2024-10-09 18:13:16 -0400

    Added in the scanner and made a new branch to fix everything.

--- interpreter/scanner.cpp
+++ interpreter/scanner.cpp
@@ -1,192 +1,202 @@
-#include "simple.cpp"
-#include "classes/headers/scanner.h"
 #include <iostream>
 #include <string>
+#include "simple.cpp"
 #include <fstream>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
-    // helper functions
-    bool Scanner::is_number(string s) { // determines if the string is a number
-        for (char c : s) {
-            if (!isdigit(c)) return false;
-        }
-        return true;
-    }
-    bool Scanner::is_letter(string s) { // determines if the string is a word
-        for (char c : s) {
-            if (!isalpha(c)) return false;
-        }
-        return true;
-    }
-    bool Scanner::is_symbol(string s) { // determines if the string is a symbol or not
-        for (char c : s) {
-            if (!isalpha(c) && !isdigit(c)) return true;
-        }
-        return false;
-    }
-    void Scanner::symbol_check(string s) { // helper function for getting symbols
-        if (s.compare("=") == 0) {
-            curr_token = ASSIGN;
-        }
-        else if (s.compare(";") == 0) {
-            curr_token = SEMICOLON;
-        }
-        else if (s.compare("(") == 0) {
-            curr_token = LPAREN;
-        }
-        else if (s.compare(")") == 0) {
-            curr_token = RPAREN;
-        }
-        else if (s.compare("{") == 0) {
-            curr_token = LCURL;
-        }
-        else if (s.compare("}") == 0) {
-            curr_token = RCURL;
-        }
-        else if (s.compare("!") == 0) {
-            curr_token = NOT;
-        }
-        else if (s.compare("|") == 0) {
-            curr_token = OR;
-        }
-        else if (s.compare("&") == 0) {
-            curr_token = AND;
-        }
-        else if (s.compare("<") == 0) {
-            curr_token = LESS;
-        }
-        else if (s.compare("+") == 0) {
-            curr_token = ADD;
-        }
-        else if (s.compare("-") == 0) {
-            curr_token = SUBTRACT;
-        }
-        else if (s.compare("*") == 0) {
-            curr_token = MULTIPLY;
-        }
-        else if (s.compare("/") == 0) {
-            curr_token = DIVIDE;
-        }
-        else {
-            cerr << "ERROR: Invalid syntax error!" << endl;
-            exit(-1);
-        }
-    }
-    bool Scanner::keyword_check(string s) { // check if there are any valid keywords, return false if not
-        bool found = true;
-        if (s.compare("program") == 0) {
-            curr_token = PROGRAM;
-        }
-        else if (s.compare("out") == 0) {
-            curr_token = PRINT;
-        }
-        else if (s.compare("if") == 0) {
-            curr_token = IF;
-        }
-        else if (s.compare("else") == 0) {
-            curr_token = ELSE;
-        }
-        else if (s.compare("while") == 0) {
-            curr_token = WHILE;
-        }
-        else if (s.compare("num") == 0) {
-            curr_token = NUM;
-        }
-        else found = false;
-        return found;
-    }
+#ifndef SCANNER_H
+#define SCANNER_H
 
-    Scanner::Scanner(string f) : char_pointer(0), curr_number(0), curr_name("") {
-        ifstream fstrm("test.txt");
-        string line;
-        if (!fstrm.is_open()) {
-            cerr << "ERROR: File failed to open!" << endl;
-            return;
-        }
-        else {
-            while (getline(fstrm, line)) { // get all characters from the stream
-                for (char c : line) {
-                    char_stream.push_back(c);
-                }
-            }
-            fstrm.close();
-        }
-    }
+class Scanner {
+    private:
 
-    simple Scanner::current_token() {
-        return curr_token;
-    }
+        // you may either keep an array or a list of all characters in the stream, or...
+        // ...keep the file stream open, and read only characters that you need at the moment!
 
-    void Scanner::next_token() {
-        string token_string = "";
-        char token_c;
-        while (curr_token != EOS) {
-            token_c = char_stream[char_pointer];
-            if (static_cast<size_t>(char_pointer) != char_stream.size() && token_c != ' ' && token_c != '\n' && token_c != '\0' && token_c != '\t') { // checking for any potential whitespace
-                token_string += string(1, token_c); // if not whitespace, append to string
-                if (!isdigit(char_stream[char_pointer + 1]) && !isalpha(char_stream[char_pointer + 1]) && keyword_check(token_string)) {
-                    // if the current word looks like a keyword and has nothing leading it, it's a keyword
-                    char_pointer++;
-                    break;
-                }
-                else if ((!isdigit(char_stream[char_pointer + 1]) && !isalpha(char_stream[char_pointer + 1])) || is_symbol(token_string)) { // check if we still have valid chars ahead
-                    if (is_number(token_string)) { // check if a full number
-                        curr_token = NUMBER;
-                        curr_number = stoi(token_string);
-                        if (curr_number < 0 || curr_number > 1000000) { // check for too big
-                            cerr << "ERROR: NUMBER value too large!" << endl;
-                            exit(-1);
+        simple curr_token; // the current token
+        int curr_number; // the int value of the most recent (or current) 'NUMBER' token
+        string curr_name; // the string value of the most recent (or current) 'NAME' token
+        queue<string> q;
+
+
+    public:
+        Scanner(string f) {
+            // use filename argument 'f' to open the file, read characters as needed, and eventually close
+            // the file stream
+
+            ifstream file(f);
+
+            if(file.is_open()){
+                cout << "Whoopie it opened!" << endl;
+                queue<string> q;    //Make a new queue. Parently this is it....
+                
+                //now read in any available input.
+                string line;
+                string token = "";
+                int tokenSize = 0;
+                while(getline(file, line)){
+                    //line now holds a line of input from the file.
+
+                    //go over each character in the line.
+                    for(int i = 0; i < line.length(); i++){
+                        char c = line[i];
+
+                        //check if the character is part of the current token.
+                        //TODO check this
+                        if(c != ' '){
+                            if((c == '(') || (c == '{')){
+                                //this is the case of a special circ. token!
+                                q.push(""+c); //Push the (/{ as it's own token!
+                            }else if((c == ')') || (c == '}')){
+                                //the }/) came at the end of something else...
+                                if(token.size() > 0){
+                                    q.push(token);
+                                    token = "";
+                                    tokenSize = 0;
+                                }
+                                q.push(""+c); //end of the token...
+                            }else{
+                                token += c;
+                                tokenSize++;
+                            }
+                            
+                        }else{
+                            //if not, we have a new token to get!
+                            q.push(token);
+                            token = "";
+                            tokenSize = 0;
                         }
-                        char_pointer++;
+                    }
+                }
+
+                //By here all words in the file have been turned to tokens...
+
+                //check for leftover tokens if the file ended.
+                if(token != ""){
+                    q.push(token);
+                }
+
+            }else{
+                cerr << "Sucks to suck, the file didn't open." << endl;
+                exit(1);
+            }
+
+            // if we cannot find the specified file, gracefully return an error and exit
+        }
+
+        simple current_token() {
+            // all you have to do here is return the currently stored token without going to the next one
+            return curr_token;
+        }
+
+        int is_num(string token){
+            int num = 1;
+            string numbers = "0123456789";
+                for(char c : token){
+                    if (!numbers.find(c)){
+                        num = 0;
                         break;
                     }
-                    else if (is_symbol(token_string)) {
-                        if (token_c == '=' && char_stream[char_pointer + 1] == '=') { // i only need to check '==', otherwise it's one character symbol
-                            curr_token = EQUAL;
-                            char_pointer += 2;
-                            break;
-                        }
-                        else {
-                            symbol_check(token_string);
-                            char_pointer++;
-                            break;
-                        }
-                    }
-                    else { // anything else is a variable name
+                }
+                if(num){
+                    curr_token = NUMBER;
+                    curr_number = stoi(token);
+                }
+            return num;
+        }
+
+        void next_token() {
+            // find the next available token in the scanning stream, covert it into a token, store it
+            // if a character is an invalid symbol, throw an error
+            // NOTE: this will be your longest method!
+
+            if(q.size() > 0){
+                
+                //in c, one must look at the item first to get the ref, then pop it off the front!
+                string token = q.front();
+                q.pop();
+
+                //first, check if the token is a number...
+                int num = is_num(token);
+
+                if(!num){
+                    //next, check for the keywords...
+                    if(token == "PROGRAM"){
+                        curr_token = PROGRAM;
+                    }else if(token == "="){
+                        curr_token = ASSIGN;
+                    }else if(token == ";"){
+                        curr_token = SEMICOLON;
+                    }else if(token == "say"){
+                        curr_token = PRINT;
+                    }else if(token == "if"){
+                        curr_token = IF;
+                    }else if(token == "else"){
+                        curr_token = ELSE;
+                    }else if(token == "while"){
+                        curr_token = WHILE;
+                    }else if(token == "("){
+                        curr_token = LPAREN;
+                    }else if(token == ")"){
+                        curr_token = RPAREN;
+                    }else if(token == "{"){
+                        curr_token = LCURL;
+                    }else if(token == "}"){
+                        curr_token = RCURL;
+                    }else if(token == "!"){
+                        curr_token = NOT;
+                    }else if(token == "||"){
+                        curr_token = OR;
+                    }else if(token == "&&"){
+                        curr_token = AND;
+                    }else if(token == "-"){
+                        curr_token = SUBTRACT;
+                    }else if(token == "*"){
+                        curr_token = MULTIPLY;
+                    }else if(token == "/"){
+                        curr_token = DIVIDE;
+                    } else if(token == "num") {
+                        curr_token = NUM;
+                        //cmon
+                    } else {
+                        //TODO -- add more checks for these later...
                         curr_token = NAME;
-                        curr_name = token_string;
-                        char_pointer++;
-                        break;
+                        curr_name = token;
                     }
                 }
+                
+                
+            }else{
+                cerr<< "The number of tokens is 0!" << endl;
+                exit(0);
             }
-            else if (static_cast<size_t>(char_pointer) >= char_stream.size() - 1) { // get eos token here
-                curr_token = EOS;
+        }
+
+        string get_name() {
+            // if the current token is 'NAME', return the string value of the name, else throw an error
+            if(current_token() == NAME){
+                return curr_name;
+            }else{
+                printf("THE CURRENT TOKEN WAS NOT A NAME.\nEXITING PROGRAM...\n");
+                exit(0);
             }
-            char_pointer++;
         }
-    }
 
-    string Scanner::get_name() {
-        if (curr_token == NAME) {
-            return curr_name;
+        int get_number() {
+            // if the current token is 'NUMBER', return the int value of the number, else throw an error
+            if(current_token() == NUMBER){
+                return curr_number;
+            }else{
+                printf("THE CURRENT TOKEN WAS NOT A NUMBER.\nEXITING PROGRAM...\n");
+                exit(0);
+            }
         }
-        else {
-            cerr << "ERROR: Token is not of type NAME!" << endl;
-            exit(-1);
-            return "";
+        int main(){
+            Scanner s("test01.txt");
+            return 0;
         }
-    }
+};
 
-    int Scanner::get_number() {
-        if (curr_token == NUMBER) {
-            return curr_number;
-        }
-        else {
-            cerr << "ERROR: Token is not of type NUMBER!" << endl;
-            exit(-1);
-            return -1;
-        }
-    }
+#endif
